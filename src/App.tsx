baecd34a3dc41698a1af2770362f66269161109f
James Reid <james.reid@mosaicsmartdata.com>
2023-08-31 23:49:29 +0100

    Add forecasts to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { Button, Form } from "react-bootstrap";
 import "./App.css";
 import { useState } from "react";
+import fetchWeatherForecast from "./api/fetchWeatherForecast";
+
+interface WeatherForcast {
+  date: string;
+  maxTemp: number;
+  minTemp: number;
+  avgTemp: number;
+}
 
 function App() {
   const [city, setCity] = useState<string>("");
+  const [weatherForcast, setWeatherForcast] = useState<WeatherForcast[]>([]);
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setCity(e.target.value);
   }
 
-  function handleFormSubmit(e: React.FormEvent) {
+  async function handleFormSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log(city);
+
+    const weatherForecast = await fetchWeatherForecast(city);
+    console.log(weatherForecast);
+    setWeatherForcast(weatherForecast);
   }
 
   return (
@@ -20,7 +32,7 @@ function App() {
         <h1>Weather Forecast</h1>
 
         <Form>
-          <Form.Group className="formWeather" controlId="formWeather">
+          <Form.Group className="form-weather" controlId="formWeather">
             <Form.Label>City</Form.Label>
             <Form.Control
               type="text"
@@ -35,7 +47,20 @@ function App() {
           </Button>
         </Form>
 
-        <div className="forecast-container"></div>
+        <div className="forecast-container">
+          {weatherForcast.length > 0 ? (
+            weatherForcast.map((forecast, index) => (
+              <div className="forecast" key={index}>
+                <h2>{forecast.date}</h2>
+                <p>Max Temp: {forecast.maxTemp}°C</p>
+                <p>Min Temp: {forecast.minTemp}°C</p>
+                <p>Avg Temp: {forecast.avgTemp}°C</p>
+              </div>
+            ))
+          ) : (
+            <p>No weather forecast available.</p>
+          )}
+        </div>
       </header>
     </div>
   );
